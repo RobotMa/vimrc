7f5ea70496d4c21bea3e2f3aa94e2ff5aef82a95
Qianli Ma <qianli.ma622@gmail.com>
2018-01-31 00:38:15 -0800

    minor changes

--- update_plugins.py
+++ update_plugins.py
@@ -52,6 +52,7 @@ vim-pug https://github.com/digitaltoad/vim-pug
 vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
 lightline.vim https://github.com/itchyny/lightline.vim
 YouCompleteMe https://github.com/Valloric/YouCompleteMe
+vim-tmux https://github.com/tmux-plugins/vim-tmux.git
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
